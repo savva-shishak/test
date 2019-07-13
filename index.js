d94cb1aeab65a057bd97f9c86474145b51160470
Savva Shishak <sav.shishak@gmail.com>
2019-07-13 11:09:27 +0500

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const assert = require('assert')
 const rp = require('request-promise')
 
 describe('object operations', () => {
-  let data
+  let data = {"bpi":{"2019-01-01":3869.47,"2019-01-02":3941.2167,"2019-01-03":3832.155,"2019-01-04":3863.6267,"2019-01-05":3835.5983,"2019-01-06":4083.165,"2019-01-07":4041.4583,"2019-01-08":4029.9917,"2019-01-09":4028.2917,"2019-01-10":3669.5825,"2019-01-11":3660.9583,"2019-01-12":3658.6725,"2019-01-13":3540.1583,"2019-01-14":3706.1725,"2019-01-15":3604.1333,"2019-01-16":3630.2567,"2019-01-17":3665.41,"2019-01-18":3630.785,"2019-01-19":3708.9633,"2019-01-20":3552.8933,"2019-01-21":3548.69,"2019-01-22":3598.2183,"2019-01-23":3576.01,"2019-01-24":3598.2717,"2019-01-25":3582.2,"2019-01-26":3580.775,"2019-01-27":3553.0133,"2019-01-28":3454.48,"2019-01-29":3421.12,"2019-01-30":3482.3},"disclaimer":"This data was produced from the CoinDesk Bitcoin Price Index. BPI value data returned as USD.","time":{"updated":"Jan 31, 2019 00:03:00 UTC","updatedISO":"2019-01-31T00:03:00+00:00"}}
 
   before(async () => {
     /**
@@ -14,7 +14,7 @@ describe('object operations', () => {
      * - Populate the params to complete the request (you don't have to use the `url` package)
      * - Attach the response to the data variable defined above
      */
-    const params = null // TODO:
+    const params = "https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-01-01&end=2019-01-31"
 
     const result = await rp(params)
     data = result.bpi
